b998855ed9c970f7ed7c1dc4f3ce16706add5e59
miguel <miguel.amoedo.cardoso@gmail.com>
2017-06-29 13:55:06 -0300

    Exercicio-Prova

---
+++ Soluções/Exercicio-Prova/Exercicio-Prova/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Prova
+{
+    class Questao
+    {
+        public int tomA, tomB, tomC, tomD, tomE, nresp, ninv;
+        public string resp;
+
+        public void Resposta()
+        {
+
+            nresp = 0;
+            ninv = 0;
+            if (tomA <= 100)
+            {
+                resp = "A";
+                nresp++;
+            }
+            else if (tomA <= 155)
+                ninv++;
+
+            if (tomB <= 100)
+            {
+                resp = "B";
+                nresp++;
+            }
+            else if (tomB <= 155)
+                ninv++;
+
+            if (tomC <= 100)
+            {
+                resp = "C";
+                nresp++;
+            }
+            else if (tomC <= 155)
+                ninv++;
+
+            if (tomD <= 100)
+            {
+                resp = "D";
+                nresp++;
+            }
+            else if (tomD <= 155)
+                ninv++;
+
+            if (tomE <= 100)
+            {
+                resp = "E";
+                nresp++;
+            }
+            else if (tomE <= 155)
+                ninv++;
+
+            if (nresp != 1 || ninv != 0)
+                Console.WriteLine("*");
+
+            else
+                Console.WriteLine(resp);
+        }
+    }
+}
---
+++ Soluções/Exercicio-Prova/Exercicio-Prova/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, N = Int32.Parse(Console.ReadLine());
+
+            Questao[] quest = new Questao[N];
+            i = 0;
+            while (i < N)
+            {
+                quest[i] = new Questao();
+
+                quest[i].tomA = Int32.Parse(Console.ReadLine());
+                quest[i].tomB = Int32.Parse(Console.ReadLine());
+                quest[i].tomC = Int32.Parse(Console.ReadLine());
+                quest[i].tomD = Int32.Parse(Console.ReadLine());
+                quest[i].tomE = Int32.Parse(Console.ReadLine());
+
+                i++;
+            }
+
+            i = 0;
+            while (i < N)
+            {
+                quest[i].Resposta();
+                i++;
+            }
+        }
+    }
+}
