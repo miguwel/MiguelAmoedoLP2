7fab10e8955b4be7517ce159fb3ee8bb765e13ea
miguel <miguel.amoedo.cardoso@gmail.com>
2017-08-01 15:58:35 -0300

    Solução parcial do execicio de conversão de temp

---
+++ 2C/3-ProvaRec/2-ProvaRec/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_ProvaRec
+{
+    class TempC
+    {
+        public double valor;
+
+        public double EmF()
+        {
+            return 1.8 * valor + 32;
+        }
+
+        public double EmK()
+        {
+            return valor + 273;
+        }
+    }
+}
---
+++ 2C/3-ProvaRec/2-ProvaRec/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_ProvaRec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempC temp = new TempC();
+            
+            int op = 42;
+
+            while (op != 0)
+            {
+                Console.WriteLine("1 - Nova Temperatura");
+                Console.WriteLine("2 - Para Fahrenheit");
+                Console.WriteLine("3 - Para Kelvin");
+                Console.WriteLine("0 - Sair");
+
+                op = int.Parse(Console.ReadLine());
+
+                if(op == 1)
+                    temp.valor = double.Parse(Console.ReadLine());
+
+                if(op == 2)
+                    Console.WriteLine(temp.EmF());
+
+                if(op == 3)
+                    Console.WriteLine(temp.EmK());          
+            }
+        }
+    }
+}
