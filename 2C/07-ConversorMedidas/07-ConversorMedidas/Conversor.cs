93bd59f6a5ad337f55dc40352cdb65affdd8d6f9
Miguel Amoedo <miguel.amoedo.cardoso@gmail.com>
2017-08-31 14:25:45 -0300

    Conversor Feito

---
+++ 2C/07-ConversorMedidas/07-ConversorMedidas/Conversor.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ConversorMedidas
+{
+    static class Conversor
+    {
+        public static double KgParaG(double x)
+        {
+            return x * 1000;
+        }
+
+        public static double GParaKg(double x)
+        {
+            return x / 1000;
+        }
+
+        public static double KgParaT(double x)
+        {
+
+            return x * 1000;
+        }
+
+        public static double TParaKg(double x)
+        {
+            return x / 1000;
+        }
+
+        public static double KgParaLb(double x)
+        {
+            return x * 2.2046;
+        }
+
+        public static double LbParaKg(double x)
+        {
+            return x / 2.2046;
+        }
+
+        public static double CParaF(double x)
+        {
+            return x * 1.8 + 32;
+        }
+
+        public static double FParaC(double x)
+        {
+            return (x - 32) / 1.8;
+        }
+
+        public static double CParaK(double x)
+        {
+            return x + 273;
+        }
+
+        public static double KParaC(double x)
+        {
+            return x - 273;
+        }
+
+        public static double FParaK(double x)
+        {
+            x = FParaC(x);
+            return CParaK(x);
+        }
+
+        public static double KParaF(double x)
+        {
+            x = KParaC(x);
+            return CParaF(x);
+        }
+
+        public static double MParaKm(double x)
+        {
+            return x / 1000;
+        }
+
+        public static double KmParaM(double x)
+        {
+            return x * 1000;
+        }
+
+        public static double MParaPes(double x)
+        {
+            return x * 3.2808;
+        }
+
+        public static double PesParaM(double x)
+        {
+            return x / 3.2808;
+        }
+
+        public static double PolParaPes(double x)
+        {
+            return x * 0.083333;
+        }
+
+        public static double PesParaPol(double x)
+        {
+            return x * 12.000;
+        }
+
+        public static double MParaPol(double x)
+        {
+            return x * 39.370;
+        }
+
+        public static double PolParaM(double x)
+        {
+            return x / 39.370;
+        }
+
+        public static double MilhaParaKm(double x)
+        {
+            return x / 0.62137;
+        }
+
+        public static double KmParaMilha(double x)
+        {
+            return x * 0.62137;
+        }
+
+        
+    }
+}
---
+++ 2C/07-ConversorMedidas/07-ConversorMedidas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ConversorMedidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 42;
+
+            while (op != 0)
+            {
+                Console.WriteLine("1 - Kilometros para Milhas");
+                Console.WriteLine("2 - Milhas para Kilometros");
+                Console.WriteLine("3 - Celsius para Fahrenheit");
+                Console.WriteLine("4 - Fahrenheit para Celsius");
+                Console.WriteLine("5 - Kilogramas para Libras");
+                Console.WriteLine("6 - Libras para Kilogramas");
+                Console.WriteLine("0 - Sair");
+                op = int.Parse(Console.ReadLine());
+
+                if (op == 1)
+                    Console.WriteLine(Conversor.KmParaMilha(double.Parse(Console.ReadLine())));
+
+                else if (op == 2)
+                    Console.WriteLine(Conversor.MilhaParaKm(double.Parse(Console.ReadLine())));
+
+                else if (op == 3)
+                    Console.WriteLine(Conversor.CParaF(double.Parse(Console.ReadLine())));
+
+                else if (op == 4)
+                    Console.WriteLine(Conversor.FParaC(double.Parse(Console.ReadLine())));
+
+                else if (op == 5)
+                    Console.WriteLine(Conversor.KgParaLb(double.Parse(Console.ReadLine())));
+
+                else if (op == 6)
+                    Console.WriteLine(Conversor.LbParaKg(double.Parse(Console.ReadLine())));                   
+            }
+
+        }
+    }
+}
