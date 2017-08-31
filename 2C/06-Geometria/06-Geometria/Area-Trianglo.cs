e2d4d4f804998089b9e6a844a126f0d3990d8783
miguel.amoedo.cardoso@gmail.com <miguel.amoedo.cardoso@gmail.com>
2017-08-31 17:34:37 -0300

    Conversor Pronto

---
+++ 2C/06-Geometria/06-Geometria/Area-Trianglo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Geometria
+{
+    class Triangulo
+    {
+
+        public double A { get; set; }
+        public double B { get; set; }
+        public int O { get; set; }
+
+        private double area;
+
+        public double Area
+        {
+            get { return area; }
+        }
+
+        private double perimetro;
+
+        public double Perimetro
+        {
+            get { return perimetro; }
+        }
+
+        public double PerimetroTriangulo()
+        {
+            this.perimetro = A + B + Math.Sqrt((A*A)+(B*B));
+            return Perimetro;
+        }
+
+        public double AreaTriangulo(double b, double H)
+        {
+            this.area = (B * (A * Math.Sin(O))) / 2;
+            return Area;
+        }
+
+        public double AreaTriangulo(double a, double b, double cosO) 
+        {
+            this.area = 2 * (A + B) * Math.Cos(O);
+            return Area;
+        }
+
+
+    }
+}
---
+++ 2C/5-Produtos/5-Produtos/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Produtos
+{
+    class Produto
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Nome { get; set; }
+
+        public double Preço { get; set; }
+
+        private int quantidade;
+        public int Quantidade
+        {
+            get { return id; }
+        }
+
+        public void Repor(int qtd)
+        {
+            quantidade += qtd;
+        }
+
+        public Produto(int id, string nome, double preço)
+        {
+            this.id = id;
+            this.Nome = nome;
+            this.Preço = preço;
+            this.quantidade = 0;
+        }
+    }
+}
