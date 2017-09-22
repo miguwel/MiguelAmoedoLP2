1429125e951ed85463b799da361355edac111437
Miguel Amoedo <miguel.amoedo.cardoso@gmail.com>
2017-09-22 08:28:00 -0300

    Codigo Morse

--- 2C/4.Codigo-Morsa/4.Codigo-Morsa/Mensagem.cs
+++ 2C/4.Codigo-Morsa/4.Codigo-Morsa/Mensagem.cs
@@ -8,20 +8,66 @@ namespace _4.Codigo_Morsa
 {
     class Mensagem
     {
-        private string[] morse = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+        public string txt { get; set; }
+        private string[] morse = new string[] 
+        { 
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." 
+        };
 
-        public string Txt { get; set; }
-        public string Codigo { get; set; }
-
-        public void Codificar()
+        private char[] letra = new char[]
         {
+            'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
 
+        public string Codificar()
+        {
+            string codigo = "", x = "";
+
+            Console.WriteLine("Codificar:");
+            txt = Console.ReadLine();
+
+            for (int i = 0; i < txt.Length; i++)
+            {
+                for (int p = 0; p < 26; p++)
+                {
+                    if (txt[i] == letra[p] || txt[i] == char.ToUpper(letra[p]))
+                    {
+                        x = morse[p];
+                    }
+                }
+
+                string cod = x + " ";
+                codigo += cod;
+            }
+
+            return codigo;
         }
 
-        public void Decodificar()
+        public string Decodificar()
         {
+            string mensagem = "";
+            char caracter = ' ';
 
+            Console.WriteLine("Decodificar:");
+            txt = Console.ReadLine();
+
+            string[] codigo = txt.Split(' ');
+
+            for (int i = 0; i < codigo.Length; i++)
+            {
+                for (int p = 0; p < 26; p++)
+                {
+                    if (codigo[i] == morse[p])
+                    {
+                        caracter = char.ToUpper(letra[p]);
+                    }
+                }
+
+                char cod = caracter;
+                mensagem += cod;
+            }
+
+            return mensagem;
         }
-        
     }
 }
--- 2C/4.Codigo-Morsa/4.Codigo-Morsa/Program.cs
+++ 2C/4.Codigo-Morsa/4.Codigo-Morsa/Program.cs
@@ -11,19 +11,24 @@ namespace _4.Codigo_Morsa
         static void Main(string[] args)
         {
 
-            int op;
+            int op = 3;
+            Mensagem msg = new Mensagem();
 
-            while(true)
+            while (op != 0)
             {
                 Console.WriteLine("1 - Codificar");
                 Console.WriteLine("2 - Decodificar");
-                Console.WriteLine("3 - Transmitir");
                 Console.WriteLine("0 - Sair");
 
-                Mensagem msg = new Mensagem();
+                op = int.Parse(Console.ReadLine());
 
-                
-            }
+                if (op == 1)
+                    Console.WriteLine(msg.Codificar());
+
+                else if (op == 2)
+                    Console.WriteLine(msg.Decodificar());
+
+            } 
         }
     }
 }
