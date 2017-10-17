f6e08b1fe9fc8ad058adb85a911b3c0918b6048e
miguel.amoedo2001@gmail.com <migulei12>
2017-10-17 15:46:48 -0300

    LP2-BD-Exemplo

---
+++ Pessoal/LP2-BD-Exemplo/LP2-BD-Exemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP2_BD_Exemplo
+{
+    class Program,
+
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conec = new MySqlConnection("Server=localhost;Database=casa;Uid=root;Pwd=");
+
+            conec.Open();
+            // Interagir com o Banco de Dados;
+            conec.Close();
+        }
+    }
+}
