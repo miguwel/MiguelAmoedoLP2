e58531a5f8dc2a1041f5e47afa783f6328b1c6a5
miguel <miguel.amoedo.cardoso@gmail.com>
2017-11-07 15:31:19 -0300

    ridicul

--- Pessoal/LP2-BD-Exemplo/LP2-BD-Exemplo/Program.cs
+++ Pessoal/LP2-BD-Exemplo/LP2-BD-Exemplo/Program.cs
@@ -7,16 +7,21 @@ using System.Threading.Tasks;
 
 namespace LP2_BD_Exemplo
 {
-    class Program,
-
+    class Program
     {
         static void Main(string[] args)
         {
-            MySqlConnection conec = new MySqlConnection("Server=localhost;Database=casa;Uid=root;Pwd=");
+            MySqlConnection conec = new MySqlConnection("Server=localhost;Database=escola;Uid=root;Pwd=");
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.Connection = conec;
+            cmd.CommandText = "INSERT INTO Aluno(nome) VALUES ('ok');";
 
             conec.Open();
-            // Interagir com o Banco de Dados;
+            cmd.ExecuteNonQuery();
             conec.Close();
+
+            Console.WriteLine("o.k.");
         }
     }
 }
