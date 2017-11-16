9b3c34fff0689522ffb97bf20817f8185984fdf9
PaulinhaSigoDeVolta <paulavdutr@gmail.com>
2017-11-16 15:25:21 -0200

    Carro

--- Pessoal/LP2-BD-Exemplo/LP2-BD-Exemplo/Program.cs
+++ Pessoal/LP2-BD-Exemplo/LP2-BD-Exemplo/Program.cs
@@ -11,67 +11,148 @@ namespace LP2_BD_Exemplo
     {
         static void Main(string[] args)
         {
-            string stg;
-            int n, i;
-            n = 0;
-            Carro[] carro = new Carro[10000];
-            i = 1;
-            while(i != 0)
+            string stg, op1, op2, op3, op4;
+            int o, j;
+            Carro carro = new Carro();
+            o = 1;
+
+            while (o != 0)
             {
-                carro[n] = new Carro();
-                Console.WriteLine("Insira os dados do Carro: ");
-                
-                Console.Write("Marca: ");
-                carro[n].Marca = Console.ReadLine();
-
-                Console.Write("Modelo: ");
-                carro[n].Modelo = Console.ReadLine();
-
-                Console.Write("Cor: ");
-                carro[n].Cor = Console.ReadLine();
-
-                Console.Write("Placa: ");
-                carro[n].Placa = Console.ReadLine();
-
-                Console.Write("Potencia: ");
-                carro[n].Potencia = int.Parse(Console.ReadLine());
-
+                Console.WriteLine("1 - Cadastrar");
+                Console.WriteLine("2 - Deletar");
                 Console.WriteLine("0 - Encerrar");
-                Console.WriteLine("1 - Inserir outro Carro");
+                o = int.Parse(Console.ReadLine());
 
-                i = int.Parse(Console.ReadLine());
-                n++;
+                if (o == 1)
+                {
+                   
+                    Console.WriteLine("   Insira os dados do Carro: ");
+
+                    Console.Write("   Marca: ");
+                    carro.Marca = Console.ReadLine();
+
+                    Console.Write("   Modelo: ");
+                    carro.Modelo = Console.ReadLine();
+
+                    Console.Write("   Cor: ");
+                    carro.Cor = Console.ReadLine();
+
+                    Console.Write("   Placa: ");
+                    carro.Placa = Console.ReadLine();
+
+                    Console.Write("   Potencia: ");
+                    carro.Potencia = int.Parse(Console.ReadLine());
+
+                    SqlConnection conec = new SqlConnection("Data Source=localhost;Initial Catalog=LPAula;Integrated Security=SSPI");
+                    SqlCommand cmd = new SqlCommand();
+
+                    cmd.Connection = conec;
+                    conec.Open();
+
+                    cmd.Parameters.AddWithValue("Marca", carro.Marca);
+                    cmd.Parameters.AddWithValue("Modelo", carro.Modelo);
+                    cmd.Parameters.AddWithValue("Cor", carro.Cor);
+                    cmd.Parameters.AddWithValue("Placa", carro.Placa);
+                    cmd.Parameters.AddWithValue("Pot", carro.Potencia);
+
+                    stg = string.Format(@"INSERT 
+                                        INTO Carro(Marca , Modelo , Cor , Placa , Potencia) 
+                                        VALUES (@Marca, @Modelo, @Cor, @Placa, @Pot);");
+                    cmd.CommandText = stg;
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Parameters.RemoveAt("Marca");
+                    cmd.Parameters.RemoveAt("Modelo");
+                    cmd.Parameters.RemoveAt("Cor");
+                    cmd.Parameters.RemoveAt("Placa");
+                    cmd.Parameters.RemoveAt("Pot");
+
+                    conec.Close();
+
+                    Console.WriteLine("Efetuado com sucesso");
+                }
+
+                if(o == 2)
+                {
+                    Console.WriteLine(" 1 - Por ID");
+                    Console.WriteLine(" 2 - Por Potencia");
+                    Console.WriteLine(" 3 - Por Modelo e Cor");
+                    j = int.Parse(Console.ReadLine());
+                    
+                    if(j == 1)
+                    {
+                        Console.Write("   Digite o ID: ");
+                        op1 = Console.ReadLine();
+
+                        SqlConnection conec = new SqlConnection("Data Source=localhost;Initial Catalog=LPAula;Integrated Security=SSPI");
+                        SqlCommand cmd = new SqlCommand();
+
+                        cmd.Connection = conec;
+
+                        cmd.Parameters.AddWithValue("id", op1);
+                        
+                        conec.Open();
+                        cmd.CommandText = @"DELETE FROM Carro
+                                            WHERE Id = @id";
+                        cmd.ExecuteNonQuery();
+                        conec.Close();
+
+                        cmd.Parameters.RemoveAt("id");
+
+                        Console.WriteLine("Efetuado com sucesso");
+                    }
+
+                    if (j == 2)
+                    {
+                        Console.Write("   Digite a Potencia Minima: ");
+                        op2 = Console.ReadLine();
+
+                        SqlConnection conec = new SqlConnection("Data Source=localhost;Initial Catalog=LPAula;Integrated Security=SSPI");
+                        SqlCommand cmd = new SqlCommand();
+
+                        cmd.Connection = conec;
+
+                        cmd.Parameters.AddWithValue("pot", op2);
+
+                        conec.Open();
+                        cmd.CommandText = @"DELETE FROM Carro
+                                            WHERE Potencia <= @pot";
+                        cmd.ExecuteNonQuery();
+                        conec.Close();
+
+                        cmd.Parameters.RemoveAt("pot");
+
+                        Console.WriteLine("Efetuado com sucesso");
+                    }
+
+                    if (j == 3)
+                    {
+                        Console.Write("   Digite o Modelo: ");
+                        op3 = Console.ReadLine();
+                        Console.Write("   Digite a Cor: ");
+                        op4 = Console.ReadLine();
+
+                        SqlConnection conec = new SqlConnection("Data Source=localhost;Initial Catalog=LPAula;Integrated Security=SSPI");
+                        SqlCommand cmd = new SqlCommand();
+
+                        cmd.Connection = conec;
+
+                        cmd.Parameters.AddWithValue("Mod", op3);
+                        cmd.Parameters.AddWithValue("Cor", op4);
+
+                        conec.Open();
+                        cmd.CommandText = @"DELETE FROM Carro
+                                            WHERE Modelo = @Mod AND Cor = @Cor";
+                        cmd.ExecuteNonQuery();
+                        conec.Close();
+
+                        cmd.Parameters.RemoveAt("Mod");
+                        cmd.Parameters.RemoveAt("Cor");
+
+                        Console.WriteLine("Efetuado com sucesso");
+                    }
+                }
             }
-
-            SqlConnection conec = new SqlConnection("Data Source=localhost;Initial Catalog=LPAula;Integrated Security=SSPI");
-            SqlCommand cmd = new SqlCommand();
-
-            cmd.Connection = conec;
-            conec.Open();
-
-            for (i = 0; i < n; i++) 
-            {
-                cmd.Parameters.AddWithValue("Marca", carro[i].Marca);
-                cmd.Parameters.AddWithValue("Modelo", carro[i].Modelo);
-                cmd.Parameters.AddWithValue("Cor", carro[i].Cor);
-                cmd.Parameters.AddWithValue("Placa", carro[i].Placa);
-                cmd.Parameters.AddWithValue("Pot", carro[i].Potencia);
-
-                stg = string.Format("INSERT INTO Carro(Marca , Modelo , Cor , Placa , Potencia) VALUES (@Marca, @Modelo, @Cor, @Placa, @Pot);");
-                cmd.CommandText = stg;
-                cmd.ExecuteNonQuery();
-
-                cmd.Parameters.RemoveAt("Marca");
-                cmd.Parameters.RemoveAt("Modelo");
-                cmd.Parameters.RemoveAt("Cor");
-                cmd.Parameters.RemoveAt("Placa");
-                cmd.Parameters.RemoveAt("Pot");
-
-            }
-       
-            conec.Close();
-
-            Console.WriteLine("SAUCE, NO KETCHUP, JUST SAUCE");
         }
     }
 }
