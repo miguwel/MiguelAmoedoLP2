2ffcf2e20864a55fc6625126fb1344045527134a
miguel <miguel.amoedo.cardoso@gmail.com>
2017-12-12 17:57:51 -0300

    Forca 'feito'

--- Pessoal/LP2-WPF-Forca/Wpf_Forca/Wpf_Forca/MainWindow.xaml.cs
+++ Pessoal/LP2-WPF-Forca/Wpf_Forca/Wpf_Forca/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Wpf_Forca
     /// </summary>
     public partial class MainWindow : Window
     {
-        string chave = "";
+        string In = "";
         string c = "";
         int vidas = 5;
         public MainWindow()
@@ -30,63 +30,205 @@ namespace Wpf_Forca
 
         private void btn_In_Click(object sender, RoutedEventArgs e)
         {
-            chave = txt_In.Text;
+            In = txt_In.Text;
             txt_In.Clear();
-            for(int i = 0; i < chave.Length; i++)
+            for(int i = 0; i < In.Length; i++)
             {
-                if (chave[i] != ' ')
+                if (In[i] != ' ')
                     c = c + "*";
 
                 else
                     c = c + " ";
             }
 
-            txt_In.Text = c;
-            txt_vidas.Text = string.Format("Tens mais {0} tentativas", vidas);
+            txt_Vidas.Text = Convert.ToString(vidas);
+            txt_In.Text = string.Empty;
+            txt_string.Text = c;
         }
 
-        private void btn_A_Click(object sender, RoutedEventArgs e)
+        private void verificarLetra(char letra)
         {
             int test = 0;
             int i = 0;
             char[] x = new char[c.Length];
             x = c.ToCharArray(0, c.Length);
-            while (i < chave.Length)
+            while (i < In.Length)
             {
-                if (chave[i] == 'a' || chave[i] == 'A')
+                if (char.ToUpper(In[i]) == letra)
                 {
-                    x[i] = 'A';
+                    x[i] = letra;
                     test = 1;
                 }
+                i++;
             }
-            c = x.ToString();
+            c = new string(x);
 
             if (test == 0)
                 vidas--;
 
-            txt_In.Text = c;
+            txt_Vidas.Text = vidas.ToString();
+            txt_string.Text = c;
+        }
+       
+       //-----------------------------------------------------------------------------------------------------------------------------------------
+        private void btn_A_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('A');
+            btn_A.IsEnabled = false;
         }
 
         private void btn_B_Click(object sender, RoutedEventArgs e)
         {
-            int test = 0;
-            int i = 0;
-            char[] x = new char[c.Length];
-            x = c.ToCharArray(0,c.Length);
-            while (i < chave.Length)
-            {
-                if (chave[i] == 'b' || chave[i] == 'B')
-                {
-                    x[i] = 'B';
-                    test = 1;
-                }
-            }
-            c = x.ToString();
-
-            if (test == 0)
-                vidas--;
-
-            txt_In.Text = c;
+            verificarLetra('B');
+            btn_A.IsEnabled = false;
         }
+
+        private void btn_C_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('C');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_D_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('D');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_E_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('E');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_F_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('F');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_G_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('G');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_H_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('H');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_I_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('I');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_J_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('J');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_K_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('K');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_L_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('L');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_M_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('M');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_N_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('N');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_O_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('O');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_P_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('P');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_Q_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('Q');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_R_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('R');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_S_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('S');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_T_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('T');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_U_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('U');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_V_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('V');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_W_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('W');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_X_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('X');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_Y_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('Y');
+            btn_A.IsEnabled = false;
+        }
+
+        private void btn_Z_Click(object sender, RoutedEventArgs e)
+        {
+            verificarLetra('Z');
+            btn_A.IsEnabled = false;
+        }
+
+        
+
+        
     }
 }
